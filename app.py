0a9d4f48320236c17c72629c1185fe04438fa887
daredevil007-aks <chourasiaakshat2@gmail.com>
2024-05-14 02:42:43 +0530

    added forms for front end

---
+++ app.py
@@ -0,0 +1,22 @@
+from crypt import methods
+from flask import Flask, request,render_template, jsonify
+from src.pipeline.prediction_pipeline import CustomData, PredictPipeline
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/predict', methods['GET', 'POST'])
+
+def predict_datapoint():
+    if request.method=="GET":
+        return render_template('form.html')
+
+
+    else:
+        data=CustomData(
+            
+        )
